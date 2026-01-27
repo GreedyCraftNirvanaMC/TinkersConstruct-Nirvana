de0062fd8611cd0ca9a81ea78d3e378cd75a4424
Liquor <hz_124@163.com>
2026-01-27 10:59:29 +0800

    Initial commit

---
+++ src/main/java/com/gctn/tconstruct/TinkersConstructNirvana.java
@@ -0,0 +1,25 @@
+package com.gctn.tconstruct;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(TinkersConstructNirvana.MODID)
+public class TinkersConstructNirvana {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "tconstruct";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TinkersConstructNirvana(IEventBus modEventBus, ModContainer modContainer) {
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (TinkersConstructNirvana) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        // NeoForge.EVENT_BUS.register(this);
+
+    }
+}
